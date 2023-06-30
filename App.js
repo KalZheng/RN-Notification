6a35d697351b9bb65bd7cce585a97d74bab3653d
Kal Zheng <kal.zheng@inspire-dt.com>
2023-06-30 16:43:49 +0800

    add local notifcation

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  function scheduleNotificationHandler() {
+    try {
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: "My first local Notification",
+          body: "This is the body of the notification.",
+          data: { userName: "testUserName" },
+        },
+        trigger: {
+          seconds: 5,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Schedule Notification"
+        onPress={scheduleNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +42,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
