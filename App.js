b5ca0eb4c03641e5e6600bc669e93e0a085b526f
Kal Zheng <kal.zheng@inspire-dt.com>
2023-07-01 09:28:41 +0800

    add push notification and functions

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Alert, Button, Platform, StyleSheet, View } from "react-native";
 import * as Notifications from "expo-notifications";
 
 Notifications.setNotificationHandler({
@@ -13,21 +13,55 @@ Notifications.setNotificationHandler({
 
 export default function App() {
   useEffect(() => {
+    async function configurePushNotifications() {
+      if (Platform.OS === "android") {
+        await Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.MAX,
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: "#FF231F7C",
+        });
+      }
+
+      const { status } = await Notifications.getPermissionsAsync();
+      let finalStatus = status;
+
+      if (finalStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+
+      if (finalStatus !== "granted") {
+        Alert.alert(
+          "Permission required",
+          "Push notifications need the appropriate permissions"
+        );
+        return;
+      }
+
+      const pushTokenData = await Notifications.getExpoPushTokenAsync();
+      console.log(pushTokenData);
+    }
+
+    configurePushNotifications();
+  }, []);
+
+  useEffect(() => {
     const subscription1 = Notifications.addNotificationReceivedListener(
       (notification) => {
-        console.log("Notification Received");
-        console.log(notification);
+        // console.log("Notification Received");
+        // console.log(notification);
         const userName = notification.request.content.data.userName;
-        console.log(userName);
+        // console.log(userName);
       }
     );
 
     const subscription2 = Notifications.addNotificationResponseReceivedListener(
       (response) => {
-        console.log("Notification Response Received");
-        console.log(response);
+        // console.log("Notification Response Received");
+        // console.log(response);
         const userName = response.notification.request.content.data.userName;
-        console.log(userName);
+        // console.log(userName);
       }
     );
 
@@ -37,20 +71,16 @@ export default function App() {
     };
   }, []);
   function scheduleNotificationHandler() {
-    try {
-      Notifications.scheduleNotificationAsync({
-        content: {
-          title: "My first local Notification",
-          body: "This is the body of the notification.",
-          data: { userName: "testUserName" },
-        },
-        trigger: {
-          seconds: 2,
-        },
-      });
-    } catch (error) {
-      console.log(error);
-    }
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first local Notification",
+        body: "This is the body of the notification.",
+        data: { userName: "testUserName" },
+      },
+      trigger: {
+        seconds: 2,
+      },
+    });
   }
 
   return (
