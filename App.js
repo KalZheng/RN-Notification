9f8ff95c8296c45a24693289f77c3accc22b246c
Kal Zheng <kal.zheng@inspire-dt.com>
2023-07-01 09:56:23 +0800

    add function for push notification to phone via ExponentPushTOken

--- App.js
+++ App.js
@@ -49,8 +49,8 @@ export default function App() {
   useEffect(() => {
     const subscription1 = Notifications.addNotificationReceivedListener(
       (notification) => {
-        // console.log("Notification Received");
-        // console.log(notification);
+        console.log("Notification Received");
+        console.log(notification);
         const userName = notification.request.content.data.userName;
         // console.log(userName);
       }
@@ -58,8 +58,8 @@ export default function App() {
 
     const subscription2 = Notifications.addNotificationResponseReceivedListener(
       (response) => {
-        // console.log("Notification Response Received");
-        // console.log(response);
+        console.log("Notification Response Received");
+        console.log(response);
         const userName = response.notification.request.content.data.userName;
         // console.log(userName);
       }
@@ -70,6 +70,7 @@ export default function App() {
       Notifications.removeNotificationSubscription(subscription2);
     };
   }, []);
+
   function scheduleNotificationHandler() {
     Notifications.scheduleNotificationAsync({
       content: {
@@ -83,11 +84,30 @@ export default function App() {
     });
   }
 
+  function sendPushNotificationHandler(){
+    fetch('https://exp.host/--/api/v2/push/send', {
+      method: "POST",
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify({
+        // normally after you request this from user you strore it to database
+        to: 'ExponentPushToken[RzE02GLQQcSbiz2EQ1mNS5]',
+        title: 'Test - sent from a device!',
+        body: 'This is a test!'
+      })
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Button
         title="Schedule Notification"
         onPress={scheduleNotificationHandler}
+      />
+      <Button
+        title="Send Push Notification"
+        onPress={sendPushNotificationHandler}
       />
       <StatusBar style="auto" />
     </View>
